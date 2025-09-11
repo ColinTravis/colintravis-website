99434ad81d30b2cb2203729436603c2233846ce2
ColinTravis <colint@cybertravis.us>
2025-09-11 15:25:19 -0500

    added text-balance to header

--- components/blocks/Header.vue
+++ components/blocks/Header.vue
@@ -2,7 +2,7 @@
   <div class="mb-9">
     <h2 v-if="subHeader" class="text-base lg:text-lg mt-9">{{ subHeader }}</h2>
     <span v-if="subHeader" class="bg-ct-blue h-1 block w-1/8 mt-2"></span>
-    <h1 v-if="header" class="mt-4 font-sans text-5xl lg:text-6xl font-semibold">{{ header }}</h1>
+    <h1 v-if="header" class="mt-4 font-sans text-5xl lg:text-6xl font-semibold text-balance">{{ header }}</h1>
     <p v-if="description" class="mt-4 font-sans">{{ description }}</p>
   </div>
 </template>
