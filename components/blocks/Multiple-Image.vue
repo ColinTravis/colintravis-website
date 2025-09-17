ad1c41a5f4831c75319f4faf39d635a4f2d68887
ColinTravis <colint@cybertravis.us>
2025-09-17 14:30:59 -0500

    Tweaking some image sizes

--- components/blocks/Multiple-Image.vue
+++ components/blocks/Multiple-Image.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="flex gap-8 flex-col lg:flex-row items-center w-full">
         <div v-for="image in images" :key="image.id" class="w-full lg:w-1/2 flex justify-center">
-            <NuxtImg v-if="image && sameHeight" provider="imgix" format="webp" :src="useImageUrl(image?.url)" width="800" height="800"
-                fit="cover" :modifiers="{ auto: 'format,compress' }" loading="lazy" :placeholder="[50, 25, 75, 5]" />
-            <NuxtImg v-else-if="image && !sameHeight" provider="imgix" format="webp" :src="useImageUrl(image?.url)" width="800"
-                fit="cover" :modifiers="{ auto: 'format,compress' }" loading="lazy" :placeholder="[50, 25, 75, 5]" />
+            <NuxtImg v-if="image && sameHeight" provider="imgix" format="webp" :src="useImageUrl(image?.url)"
+                width="800" height="800" fit="cover" :modifiers="{ auto: 'format,compress' }"
+                sizes="600px sm:50vw md:1920px" densities="x1 x2" loading="lazy" :placeholder="[50, 25, 75, 5]" />
+            <NuxtImg v-else-if="image && !sameHeight" provider="imgix" format="webp" :src="useImageUrl(image?.url)"
+                width="800" fit="cover" :modifiers="{ auto: 'format,compress' }" sizes="600px sm:50vw md:1920px"
+                densities="x1 x2" loading="lazy" :placeholder="[50, 25, 75, 5]" />
         </div>
     </div>
 </template>
