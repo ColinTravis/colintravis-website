9918f26766bff0c29131e60d86ffff14ccabc7f0
ColinTravis <colint@cybertravis.us>
2025-09-19 11:45:21 -0500

    Added some color

--- components/Intro.vue
+++ components/Intro.vue
@@ -20,7 +20,7 @@ const { data: introData } = await useAsyncData(
 <template>
   <div class="py-24 grid md:grid-cols-2 gap-4 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="flex flex-col gap-2">
-      <h1 class="text-5xl font-bold dark:text-white">{{ introData.nameBlock.name }}</h1>
+      <h1 class="text-5xl font-bold dark:text-red">{{ introData.nameBlock.name }}</h1>
       <h2 class="text-lg dark:text-gray-copy">{{ introData.nameBlock.title }} </h2>
       <p class="dark:text-gray-copy">{{ introData.nameBlock.subTitle }}</p>
       <SocialLinks />
